6c3063203252ae86f20d86a918ef024d40724c12
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-10-12 00:17:52 +0900

    feat: dotenv & project configuration

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+
+// MongoDB chaqirish
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+// 1: Kirish kodlari.
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// 2 Session larga bog'liq bo'lgan codelar yoziladi
+
+// 3 View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const mongodb = require("mongodb");
+
+// let db;
+const connectionString = process.env.MONGO_URL;
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app); // single thread shu yerda
+      let PORT = process.env.PORT || 3003;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
+  }
+);
