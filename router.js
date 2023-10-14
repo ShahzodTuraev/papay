d9f2b9dd51a7d60ec8cf09df6328fd48cf8891ac
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-10-14 12:05:02 +0900

    feat: build member controllers

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require("./controllers/memberController");
+// member related routers
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
-router.get("/", (req, res) => {
-  res.send("home page");
-});
-
+// others
 router.get("/menu", (req, res) => {
   res.send("you are in the Menu page");
 });
