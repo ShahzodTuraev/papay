6bb3cc686b0c755660eed652cefd4f83c0f7fb22
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-10-21 17:45:54 +0900

    feat: multer single image upload process

---
+++ utils/upload-multer.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const multer = require("multer");
+const uuid = require("uuid");
+
+const product_storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/products");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    const extension = path.parse(file.originalname).ext; //ext bu originalnamedagi .jpeg extentionni olib beradi.
+    const random_name = uuid.v4() + extension;
+    cb(null, random_name);
+  },
+});
+module.exports.uploadProductImage = multer({ storage: product_storage });
