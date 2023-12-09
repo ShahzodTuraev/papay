ed0886dfc7a14a3bcde1f33b73bc8e9f04c53d2d
shon <turayevshahzodbek@gmail.com>
2023-12-09 20:36:59 +0900

    feat: develop createArticle and imageInsertion api business logics

--- router.js
+++ router.js
@@ -4,6 +4,9 @@ const memberController = require("./controllers/memberController");
 const productController = require("./controllers/productController");
 const orderController = require("./controllers/orderController");
 const restaurantController = require("./controllers/restaurantController");
+const communityController = require("./controllers/communityController");
+const uploader_member = require("./utils/upload-multer")("members");
+const uploader_community = require("./utils/upload-multer")("community");
 
 /********************
  *     REST API     *
@@ -65,6 +68,20 @@ router.post(
   "/orders/edit",
   memberController.retrieveAuthMember,
   orderController.editChosenOrder
+);
+
+// Community related routers
+
+router.post(
+  "/community/image",
+  uploader_community.single("community_image"),
+  communityController.imageInsertion
+);
+
+router.post(
+  "/community/create",
+  memberController.retrieveAuthMember,
+  communityController.createArticle
 );
 
 module.exports = router;
